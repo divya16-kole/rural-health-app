c1dc7a56b62f8a7061ca1ef13301c41e263b0a59
fathimamirza2006 <fathimamirza0106@gmail.com>
2026-09-26 11:24:58 +0530

    Update healthcare map and clinic support

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\lenovo\OneDrive\Desktop\rural-health-app\.venv"
+set "VIRTUAL_ENV=C:\Users\fathi\rural-health-app\.venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
